25426fe96203052cdee1e241f38778cb95575cf0
Olesj Bilous <olesjvbilous@gmail.com>
2023-07-15 23:10:54 +0200

    update enhancer

--- src/example/reduxExampleStore.ts
+++ src/example/reduxExampleStore.ts
@@ -10,12 +10,12 @@ interface ModelMap<Id extends number | string, TModel extends Model<Id>> extends
   propertyDisplayNames: Partial<ModelPropertyDisplayNames<Id, TModel>>
 }
 
-type ModelProperties<Id extends number | string, TModel extends Model<Id>> = {
+type SettableProperties<Id extends number | string, TModel extends Model<Id>> = {
  [Key in keyof TModel]: Key extends 'id' ? never : TModel[Key]
 }
 
-interface ModelPropertySetter<Id extends number | string, TModel extends Model<Id>, Key extends keyof ModelProperties<Id, TModel>> {
-  modelId: Id
+interface InstancePropertySetter<Id extends number | string, TModel extends Model<Id>, Key extends keyof SettableProperties<Id, TModel>> {
+  instanceId: Id
   propertyKey: Key
   value: TModel[Key]
 }
@@ -28,14 +28,14 @@ interface ModelSliceArgs<Id extends number | string, TModel extends Model < Id >
   modelReducer: SliceCaseReducers<TModel>
 }
 
-function modelSliceEnhancer<Id extends number | string, TModel extends Model<Id>>({
+function modelSliceEnhancer<Id extends number | string, TModel extends Model<Id>, ReadonlyMask extends Partial<SettableProperties<Id, TModel>>>({
   name,
   displayName = name,
   displayNamePlural = name + 's',
   propertyDisplayNames = {},
   modelReducer
 }: ModelSliceArgs<Id, TModel>) {
-  const modelAdapter = createEntityAdapter<TModel>({});
+  const modelAdapter = createEntityAdapter<TModel>();
 
   const initialState : ModelMap<Id, TModel> = {
     displayName,
@@ -52,12 +52,15 @@ function modelSliceEnhancer<Id extends number | string, TModel extends Model<Id>
         modelAdapter.addOne(state as EntityState<TModel>, // https://github.com/reduxjs/redux-toolkit/issues/3596
           action.payload)
       },
-      setModelProperty(state, action: PayloadAction<ModelPropertySetter<Id, TModel, keyof ModelProperties<Id, TModel>>>) {
+      instancePropertySet(state, action: PayloadAction<InstancePropertySetter<Id, TModel, keyof Omit<SettableProperties<Id, TModel>, keyof ReadonlyMask>>>) {
+        let changes: Partial<TModel> = {};
+        changes[action.payload.propertyKey] = action.payload.value;
         modelAdapter.updateOne(state as EntityState<TModel>, {
-          id: action.payload.modelId,
-          changes: { [action.payload.propertyKey]: action.payload.value } as Partial<TModel>
+          id: action.payload.instanceId,
+          changes
         })
-      }
+      },
+      ...modelReducer
     }
   })
 }
